170d91c7102476ce352a3a1d56bc3d2676164228
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-12-17 20:58:34 +0000

    Add track command with function
    This command will query an API using this guide as an example
    https://blog.alexellis.io/golang-json-api-client/
    
    It will then extrapolate the data and present the user with the current
    price of Bitcoin. I found parsing data... hard, so I searched for an
    abstraction to make it easier. The njson package is really nice and easy
    to understand. I used this guide to help me:
    https://dev.to/m7shapan/golang-how-to-unmarshal-a-subset-of-nested-json-data-d84

---
+++ cmd/track.go
@@ -0,0 +1,109 @@
+/*
+Copyright © 2020 json.birchall@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/m7shapan/njson"
+	"github.com/spf13/cobra"
+)
+
+type Coin struct {
+	Rate string `njson:"bpi.GBP.rate"`
+}
+
+var trackCmd = &cobra.Command{
+	Use:   "track",
+	Short: "Allows you to track the rise and fall of specific coins",
+
+	// Args: cobra.MinimumNArgs(1),
+	Run: checkCoins,
+}
+
+func checkCoins(cmd *cobra.Command, args []string) {
+	data, err := queryApi()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	price, err := grabPrice(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Bitcoin:", price)
+}
+
+func queryApi() ([]byte, error) {
+	url := "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+	client := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "crypto-tracker")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body, nil
+}
+
+func grabPrice(body []byte) (string, error) {
+	var c Coin
+
+	err := njson.Unmarshal([]byte(body), &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.Rate, nil
+}
+
+func init() {
+	rootCmd.AddCommand(trackCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// trackCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// trackCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/jasonbirchall/crypto-tracker
 go 1.15
 
 require (
+	github.com/m7shapan/njson v1.0.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.0
+	github.com/tidwall/gjson v1.6.4 // indirect
+	github.com/tidwall/match v1.0.2 // indirect
 )
