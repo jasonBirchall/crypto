2fd12ae0930c0669c3f152fd3cdf9e6e9118a587
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-01-11 21:50:02 +0000

    Add additional datapoints & graph height

--- cmd/graph.go
+++ cmd/graph.go
@@ -27,20 +27,34 @@ import (
 )
 
 type Price struct {
-	DataPoint0 float64 `njson:"data.coins.0.history.18"`
-	DataPoint1 float64 `njson:"data.coins.0.history.19"`
-	DataPoint2 float64 `njson:"data.coins.0.history.20"`
-	DataPoint3 float64 `njson:"data.coins.0.history.21"`
-	DataPoint4 float64 `njson:"data.coins.0.history.22"`
-	DataPoint5 float64 `njson:"data.coins.0.history.23"`
-	DataPoint6 float64 `njson:"data.coins.0.history.24"`
-	DataPoint7 float64 `njson:"data.coins.0.history.25"`
-	DataPoint8 float64 `njson:"data.coins.0.history.26"`
+	DataPoint5  float64 `njson:"data.coins.0.history.5"`
+	DataPoint6  float64 `njson:"data.coins.0.history.6"`
+	DataPoint7  float64 `njson:"data.coins.0.history.7"`
+	DataPoint8  float64 `njson:"data.coins.0.history.8"`
+	DataPoint9  float64 `njson:"data.coins.0.history.9"`
+	DataPoint10 float64 `njson:"data.coins.0.history.10"`
+	DataPoint11 float64 `njson:"data.coins.0.history.11"`
+	DataPoint12 float64 `njson:"data.coins.0.history.12"`
+	DataPoint13 float64 `njson:"data.coins.0.history.13"`
+	DataPoint14 float64 `njson:"data.coins.0.history.14"`
+	DataPoint15 float64 `njson:"data.coins.0.history.15"`
+	DataPoint16 float64 `njson:"data.coins.0.history.16"`
+	DataPoint17 float64 `njson:"data.coins.0.history.17"`
+	DataPoint18 float64 `njson:"data.coins.0.history.18"`
+	DataPoint19 float64 `njson:"data.coins.0.history.19"`
+	DataPoint20 float64 `njson:"data.coins.0.history.20"`
+	DataPoint21 float64 `njson:"data.coins.0.history.21"`
+	DataPoint22 float64 `njson:"data.coins.0.history.22"`
+	DataPoint23 float64 `njson:"data.coins.0.history.23"`
+	DataPoint24 float64 `njson:"data.coins.0.history.24"`
+	DataPoint25 float64 `njson:"data.coins.0.history.25"`
+	DataPoint26 float64 `njson:"data.coins.0.history.26"`
 }
 
 var coin string
 
-// graphCmd represents the graph command
+// graphCmd represents the graph command. You can only return an error statement
+// using RunE.
 var graphCmd = &cobra.Command{
 	Use:   "graph",
 	Short: "Graphs the recent price points of a certain coin",
@@ -49,7 +63,8 @@ var graphCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		graph := asciigraph.Plot(data)
+
+		graph := asciigraph.Plot(data, asciigraph.Height(10))
 
 		fmt.Println(graph)
 
@@ -57,6 +72,10 @@ var graphCmd = &cobra.Command{
 	},
 }
 
+// getCoin takes a string as an argument, this is passed as a flag by the
+// graph command. It queries the API package and collates a collection of
+// float64 data type. This collection is then returned back to the command
+// function.
 func getData(coin string) ([]float64, error) {
 	var c Price
 	var arr []float64
@@ -91,14 +110,4 @@ func init() {
 
 	graphCmd.Flags().StringVarP(&coin, "coin", "c", "", "Coin to place in graph")
 	graphCmd.MarkFlagRequired("coin")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// graphCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// graphCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/spf13/viper v1.7.0
 	github.com/tidwall/gjson v1.6.4 // indirect
 	github.com/tidwall/match v1.0.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
