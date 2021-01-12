85d9b6a2a45508b27f66efa8d6b14cb957a4262a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-01-12 14:31:08 +0000

    Move tests to cmd dir and remove dedicated test dir
    This is so the code lives next to the tests. As tests are executed via
    the test ./... command, it doesn't matter if a test has it's own
    directory.

--- cmd/track_test.go
+++ cmd/track_test.go
@@ -1,4 +1,4 @@
-package test
+package cmd
 
 import (
 	"log"
@@ -7,9 +7,9 @@ import (
 	"testing"
 )
 
-const mainGo = "../../main.go"
+const mainGo = "../main.go"
 
-func TestCheckCoins(t *testing.T) {
+func TestExecute(t *testing.T) {
 
 	out, err := exec.Command("go", "run", mainGo, "track", "-c", "btc,eth").Output()
 	if err != nil {
@@ -25,5 +25,17 @@ func TestCheckCoins(t *testing.T) {
 
 	if !matched {
 		t.Error("Expected output does not match actual. Actual:", actual)
+	}
+}
+
+func TestCheckCoins(t *testing.T) {
+	coin := "btc"
+	check, err := checkCoins(coin)
+	if err != nil {
+		t.Error("Unable to query API, test failed", err)
+	}
+
+	if check == "" {
+		t.Error("Unable to query API, test failed")
 	}
 }
